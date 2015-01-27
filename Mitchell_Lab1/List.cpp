e6fe2ba9645891da91991fc16a2c49aaa4010fd6
Brent Mitchell <brent.mitchell25@gmail.com>
2015-01-26 21:04:49 -0600

    Finished implementation of all required methods

---
+++ Mitchell_Lab1/List.cpp
@@ -0,0 +1,79 @@
+/*
+ * List.cpp
+ *
+ *  Created on: Jan 26, 2015
+ *      Author: brentmitchell
+ */
+
+#include <List.h>
+
+List::List()  {
+	head = new Node("", NULL);
+}
+
+List::~List() {
+	delete head;
+}
+
+void List::insertHelper(std::string data, List::Node* n) {
+	if (n == NULL) {
+		n->data = data;
+	} else if (n->next == NULL) {
+		n->next = new Node(data, NULL);
+	} else {
+		n = n->next;
+		insertHelper(data,n);
+	}
+}
+
+List::Node* List::eraseHelper(std::string data,List::Node* n) {
+	if (n == NULL) {
+		return NULL;
+	} else if (n->data == data){
+		Node* temp = n->next;
+		delete n;
+		return temp;
+	}
+	n->next = eraseHelper(data,n->next);
+
+	return n;
+}
+
+List::Node* List::findHelper(std::string data, List::Node* n) {
+	if(n == NULL) {
+		return NULL;
+	} else if(n->data == data) {
+		return n;
+	}
+
+	return findHelper(data,n->next);
+}
+
+void List::insert(std::string data) {
+	insertHelper(data,head);
+}
+
+bool List::isEmpty() {
+	if (head == NULL) {
+		return true;
+	}
+	return false;
+}
+
+void List::erase(std::string data) {
+	head = eraseHelper(data,head);
+}
+
+void List::print() {
+	Node* iterator = head;
+	std::cout << "List: ";
+	while (iterator != NULL) {
+		std::cout << iterator->data << " ";
+		iterator = iterator->next;
+	}
+	delete iterator;
+}
+
+List::Node* List::find(std::string data) {
+	return findHelper(data,head);
+}
--- Mitchell_Lab1/lab1.cpp
+++ Mitchell_Lab1/lab1.cpp
@@ -1,17 +1,18 @@
-#include <stdio.h>
-#include <iostream>
-#include <iostream>
+#include <List.h>
 #include <fstream>
+#include <iostream>
+#include <string>
+
 using namespace std;
 int main(int argc, const char* argv[]) {
 	ifstream myfile;
-	  myfile.open (argv[1]);
-string line;
-while ( getline (myfile,line,' ') )
-    {
-      cout << line << '\n';
-    }
-	  myfile.close();
+	myfile.open(argv[1]);
+	string word;
+	List l;
+	while (getline(myfile, word, ' ')) {
+		l.insert(word);
+	}
+	myfile.close();
 
 	while (true) {
 
@@ -21,9 +22,20 @@ while ( getline (myfile,line,' ') )
 		cout << "\n3 - print\n";
 		cout << "\n4 - exit\n\n> ";
 
-		int input;
-		cin >> input;
-		if (input == 4) {
+		int choice;
+		std::string number;
+		cin >> choice;
+		if (choice == 1) {
+			cout << "\nChoose a number to be inserted to the list:\n\n> ";
+			cin >> number;
+			l.insert(number);
+		} else if (choice == 2) {
+			cout << "\nChoose a number to be deleted to the list:\n\n> ";
+			cin >> number;
+			l.erase(number);
+		} else if (choice == 3) {
+			l.print();
+		} else if (choice == 4) {
 			return 0;
 		}
 
