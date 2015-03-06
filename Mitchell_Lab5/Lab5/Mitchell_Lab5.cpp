6427fed4508bac9b86278d0f39d805b0f10d6bf5
Brent Mitchell <brent.mitchell25@gmail.com>
2015-03-05 22:16:08 -0600

    Finished Lab 5

--- Mitchell_Lab5/Lab5/Mitchell_Lab5.cpp
+++ Mitchell_Lab5/Lab5/Mitchell_Lab5.cpp
@@ -12,41 +12,41 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-	for (double i = 0.2; i <= 0.6; i += .1) {
-		for (int j = 1; j < 6; j++) {
-			ofstream file;
-			ostringstream s;
-			s << i;
+  for (double i = 0.2; i <= 0.6; i += .1) {
+    for (int j = 1; j < 6; j++) {
+      ofstream file;
+      ostringstream s;
+      s << i;
 
-			srand(j);
-			long number[table_size];
-			for (int k = 0; k < table_size; k++) {
-				number[k] = rand() % (long) (table_size * i);
-			}
+      srand(j);
+      long number[table_size];
+      for (int k = 0; k < table_size; k++) {
+	number[k] = rand() % (long) (table_size * i);
+      }
 
-			Timer t;
-			OpenHashTable *oht = new OpenHashTable(table_size);
+      Timer t;
+      OpenHashTable *oht = new OpenHashTable(table_size);
 
 
-			cout << "Open Hash Table: Load Factor = " << s.str() << " Seed = " << j << " Time = ";
+      cout << "Open Hash Table: Load Factor = " << s.str() << " Seed = " << j << " Time = ";
 
-			t.start();
-			for(int l = 0; l < table_size;l++)
-				oht->insert(number[l]);
-			t.printTime(t.stop());
-			delete oht;
+      t.start();
+      for(int l = 0; l < table_size;l++)
+	oht->insert(number[l]);
+      t.printTime(t.stop());
+      delete oht;
 
-			ClosedHashTable *cht = new ClosedHashTable(table_size);
-			cout << "Closed Hash Table: Load Factor = " << s.str() << " Seed = " << j << " Time = ";
-			Timer t2;
-			t2.start();
-			for(int l = 0; l < table_size;l++)
-				cht->insert(number[l]);
-			t2.printTime(t2.stop());
-			delete cht;
+      ClosedHashTable *cht = new ClosedHashTable(table_size);
+      cout << "Closed Hash Table: Load Factor = " << s.str() << " Seed = " << j << " Time = ";
 
-		}
-	}
+      t.start();
+      for(int l = 0; l < table_size;l++)
+	cht->insert(number[l]);
+      t.printTime(t.stop());
+      delete cht;
 
-	return 0;
+    }
+  }
+
+  return 0;
 }
