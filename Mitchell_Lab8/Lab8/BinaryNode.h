678841f81e919029086e02ca3d3c2742d86be98e
brentmitchell25 <brent.mitchell25@gmail.com>
2015-03-30 08:49:31 -0500

    Added merge, destory, and traversals.

--- Mitchell_Lab8/Lab8/BinaryNode.h
+++ Mitchell_Lab8/Lab8/BinaryNode.h
@@ -5,7 +5,8 @@
 template<typename T>
 class BinaryNode {
 public:
-	BinaryNode(T d);
+	BinaryNode(T k);
+	BinaryNode(T k, int ra, BinaryNode<T> *l, BinaryNode<T> *r);
 	T key;
 	int rank;
 	BinaryNode* left;
@@ -15,6 +16,11 @@ public:
 template<typename T>
 BinaryNode<T>::BinaryNode(T k) :
 		key(k), rank(0), left(NULL), right(NULL) {
+}
+
+template<typename T>
+BinaryNode<T>::BinaryNode(T k, int ra, BinaryNode<T> *l, BinaryNode<T> *r) :
+		key(k), rank(ra), left(l), right(r) {
 }
 
 #endif /* BINARYNODE_H_ */
--- Mitchell_Lab8/Lab8/MinLefistHeap.h
+++ Mitchell_Lab8/Lab8/MinLefistHeap.h
@@ -8,14 +8,19 @@
 #ifndef MINLEFISTHEAP_H_
 #define MINLEFISTHEAP_H_
 #include "BinaryNode.h"
+#include "Queue.h"
 
 template<typename T>
 class MinLefistHeap {
 
 private:
-	void merge(BinaryNode<T> *h1,BinaryNode<T> *h2);
+	void destroy(BinaryNode<T> *node);
+	BinaryNode<T>* merge(BinaryNode<T> *h1, BinaryNode<T> *h2);
 	int rank(BinaryNode<T> *h1, int count);
-	void swap(T& a, T& b);
+	void swap(BinaryNode<T>*& a, BinaryNode<T>*& b);
+	BinaryNode<T>* head;
+	void inorderTraversal(BinaryNode<T> *node);
+	void preorderTraversal(BinaryNode<T> *node);
 public:
 	MinLefistHeap();
 	virtual ~MinLefistHeap();
@@ -27,18 +32,29 @@ public:
 };
 
 template<typename T>
-MinLefistHeap<T>::MinLefistHeap() {
+MinLefistHeap<T>::MinLefistHeap() :
+		head(NULL) {
 
 }
 
 template<typename T>
 MinLefistHeap<T>::~MinLefistHeap() {
+	destroy(head);
+}
 
+template<typename T>
+void MinLefistHeap<T>::destroy(BinaryNode<T> *node) {
+	if (node == NULL)
+		return;
+	destroy(node->left);
+	destroy(node->right);
+	delete node;
 }
 
 template<typename T>
 void MinLefistHeap<T>::insert(T data) {
-
+	BinaryNode<T> *newNode = new BinaryNode<T>(data);
+	head = merge(newNode,head);
 }
 
 template<typename T>
@@ -48,41 +64,86 @@ bool MinLefistHeap<T>::deletemin() {
 
 template<typename T>
 void MinLefistHeap<T>::preorder() {
-
+	std::cout << "preorder: ";
+	preorderTraversal(head);
 }
 
 template<typename T>
 void MinLefistHeap<T>::inorder() {
-
+	std::cout << "inorder: ";
+	inorderTraversal(head);
 }
 
 template<typename T>
 void MinLefistHeap<T>::levelorder() {
+	std::cout << "levelorder: ";
+	if (head == NULL)
+		return;
+	Queue<BinaryNode<T>*> q;
+	BinaryNode<T>* printNode = head;
+	q.enqueue(printNode);
+
+	do {
+		printNode = q.dequeue();
+		std::cout << printNode->key << " ";
+		if (printNode->left != NULL)
+			q.enqueue(printNode->left);
+		if (printNode->right != NULL)
+			q.enqueue(printNode->right);
+	} while (!q.isEmpty());
 
 }
 
 template<typename T>
-void MinLefistHeap<T>::swap(T &a, T &b) {
-	T c(a);
+void MinLefistHeap<T>::swap(BinaryNode<T>*& a, BinaryNode<T>*& b) {
+	BinaryNode<T> *temp = new BinaryNode<T>(a->key,a->rank,a->left,a->right);
 	a = b;
-	b = c;
+	b = temp;
 }
 
 template<typename T>
-void MinLefistHeap<T>::merge(BinaryNode<T> *h1,BinaryNode<T> *h2) {
-
+BinaryNode<T>* MinLefistHeap<T>::merge(BinaryNode<T> *h1, BinaryNode<T> *h2) {
+	if (h1 == NULL)
+		return h2;
+	else if (h2 == NULL)
+		return h1;
+	else if (h1->key > h2->key) {
+		swap(h1, h2);
+	}
+	h1->right = merge(h1->right, h2);
+	if (rank(h1->left, 0) < rank(h1->right, 0))
+		swap(h1->left, h1->right);
+	return h1;
 }
 
 template<typename T>
-int MinLefistHeap<T>::rank(BinaryNode<T> *h,int count) {
-	if(h == NULL)
+int MinLefistHeap<T>::rank(BinaryNode<T> *h, int count) {
+	if (h == NULL)
 		return 0;
 	else {
 		count++;
-		return std::max(rank(h->right,count),rank(h->left,count));
+		return std::max(rank(h->right, count), rank(h->left, count));
 	}
 }
 
+template<typename T>
+void MinLefistHeap<T>::inorderTraversal(BinaryNode<T> *node) {
+	if (node == NULL)
+		return;
 
+	inorderTraversal(node->left);
+	std::cout << node->key << " ";
+	inorderTraversal(node->right);
+}
+
+template<typename T>
+void MinLefistHeap<T>::preorderTraversal(BinaryNode<T> *node) {
+	if (node == NULL)
+		return;
+
+	std::cout << node->key << " ";
+	preorderTraversal(node->left);
+	preorderTraversal(node->right);
+}
 
 #endif /* MINLEFISTHEAP_H_ */
