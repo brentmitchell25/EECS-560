c48262d943dd1d61f6750dbbb4b90bc930e0cdbb
brentmitchell25 <brent.mitchell25@gmail.com>
2015-05-01 18:13:04 -0500

    Input now takes in all nodes

--- Mitchell_Lab11/Lab11/Mitchell_Lab11.cpp
+++ Mitchell_Lab11/Lab11/Mitchell_Lab11.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
+#include <sstream>
 #include "MinimumSpanningTree.h"
 #include "DisjointSet.h"
 #include "SetNode.h"
@@ -9,18 +10,33 @@ using namespace std;
 int main(int argc, char* argv[]) {
 	ifstream myfile;
 
-		// If no command line argument, open hard coded file
-		if (argc > 1)
-			myfile.open(argv[1]);
-		else
-			myfile.open("data.txt");
-		string word;
+	// If no command line argument, open hard coded file
+	if (argc > 1)
+		myfile.open(argv[1]);
+	else
+		myfile.open("data.txt");
+	string word;
+	getline(myfile, word, '\n');
+	int testCases = atoi(word.c_str());
+	int nodes = 0;
+	MinimumSpanningTree<int>* mst = new MinimumSpanningTree<int>();
 
-		MinimumSpanningTree<int>* mst = new MinimumSpanningTree<int>();
-		while (!myfile.eof()) {
-			getline(myfile, word, ' ');
-			mst->insert(atoi(word.c_str()));
-		}
-		myfile.close();
+	for (int i = 0; i < testCases; i++) {
+		int j = 0;
+		getline(myfile, word, '\n');
+		nodes = atoi(word.c_str());
+		do {
+			getline(myfile, word, '\n');
+			//mst->insert(atoi(word.c_str()));
+			istringstream iss(word);
+			string s;
+			while (getline(iss, s, ' ')) {
+				cout << s << endl;
+			}
+			j++;
+		} while (j <= nodes);
+
+	}
+	myfile.close();
 
 }
